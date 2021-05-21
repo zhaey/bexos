0aaeb50863ea668eb9d141737262718439bb5163
zhaey <info@zhaey.pw>
2021-05-21 15:14:33 +0200

    shift to move, ctrl for squares, crosshair

--- bexos.js
+++ bexos.js
@@ -1,82 +1,100 @@
+let isdown = false;
 
-var isdown = false;
+let x = 0;
+let y = 0;
+let x_ = 0;
+let y_ = 0;
 
-var x = 0;
-var y = 0;
-var x_ = 0;
-var y_ = 0;
-
-var img = new Image();
+const img = new Image();
 
 function begin() {
-	var img_canvas = document.getElementById('image');
-	var img_ctx = img_canvas.getContext('2d');
+    let img_canvas = document.getElementById('image');
+    let img_ctx = img_canvas.getContext('2d');
 
-	var ui_canvas = document.getElementById('canvas');
+    let ui_canvas = document.getElementById('canvas');
 
-	img.onload = function() {
-		img_canvas.width = img.width;
-		img_canvas.height = img.height;
-		img_ctx.drawImage(img, 0, 0);
-		
-		ui_canvas.width = img.width;
-		ui_canvas.height = img.height;
-	}
+    img.onload = function () {
+        img_canvas.width = img.width;
+        img_canvas.height = img.height;
+        img_ctx.drawImage(img, 0, 0);
 
-	if (location.hash) {
-		img.src = location.hash.substr(1);
-	} else {
-		img.src = 'https://picsum.photos/600/400.webp';
-	}
+        ui_canvas.width = img.width;
+        ui_canvas.height = img.height;
+    }
 
-	ui_canvas.addEventListener('pointerdown', function(event) {
-		isdown = true;
-		x = event.offsetX;
-		y = event.offsetY;
-		x_ = x;
-		y_ = y;
-	});
+    if (location.hash) {
+        img.src = location.hash.substr(1);
+    } else {
+        img.src = 'https://picsum.photos/600/400.webp';
+    }
 
-	ui_canvas.addEventListener('pointermove', function(event) {
-		if (isdown === false) {
-			return;
-		}
+    ui_canvas.addEventListener('pointerdown', function (event) {
+        isdown = true;
+        if (!event.shiftKey) {
+            x = event.offsetX;
+            y = event.offsetY;
+            x_ = x;
+            y_ = y;
+        }
+    });
 
-		x_ = event.offsetX;
-		y_ = event.offsetY;
+    ui_canvas.addEventListener('pointermove', function (event) {
+        if (isdown === false) {
+            return;
+        }
 
-		window.requestAnimationFrame(draw);
-	});
+        if (event.shiftKey) {
+            x += event.movementX;
+            x_ += event.movementX;
+            y += event.movementY;
+            y_ += event.movementY;
+        } else if (event.ctrlKey) {
+            x_ = event.offsetX;
+            y_ = y + x_ - x;
+        } else {
+            x_ = event.offsetX;
+            y_ = event.offsetY;
+        }
 
-	ui_canvas.addEventListener('pointerup', function(event) {
-		if (isdown === false) {
-			return;
-		}
-		isdown = false;
-		
-		window.requestAnimationFrame(draw);
-		//alert(x + ' ' + y + ' ' + w + ' ' + h);
-	});
+        window.requestAnimationFrame(draw);
+    });
+
+    ui_canvas.addEventListener('pointerup', function (event) {
+        if (isdown === false) {
+            return;
+        }
+        isdown = false;
+
+        window.requestAnimationFrame(draw);
+    });
 
 }
 
 function draw() {
-	var canvas = document.getElementById('canvas')
-	var ctx = canvas.getContext('2d');
-	var text = document.getElementById('text');
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    let text = document.getElementById('text');
 
-	ctx.globalCompositeOperation = 'destination-over';
-	ctx.clearRect(0, 0, img.width, img.height);
-	
-	ctx.lineWidth = 1;
-	ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-	ctx.strokeStyle = 'rgba(255, 255, 255, 1.0)';
-	ctx.fillRect(x, y, x_ - x, y_ - y);
-	ctx.strokeRect(x, y, x_ - x, y_ - y);
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.clearRect(0, 0, img.width, img.height);
 
-	x_min = Math.min(x, x_)
-	x_max = Math.max(x, x_)
-	y_min = Math.min(y, y_)
-	y_max = Math.max(y, y_)
-	text.value = x_min + ' ' + y_min + ' ' + (x_max - x_min) + ' ' + (y_max - y_min);
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
+    ctx.strokeStyle = 'rgba(255, 255, 255, 1.0)';
+    ctx.fillRect(x, y, x_ - x, y_ - y);
+    ctx.strokeRect(x, y, x_ - x, y_ - y);
+
+    let x_min = Math.min(x, x_);
+    let x_max = Math.max(x, x_);
+    let y_min = Math.min(y, y_);
+    let y_max = Math.max(y, y_);
+
+    ctx.beginPath();
+    ctx.moveTo(x_min, (y_min + y_max) / 2);
+    ctx.lineTo(x_max, (y_min + y_max) / 2);
+    ctx.moveTo((x_min + x_max) / 2, y_min);
+    ctx.lineTo((x_min + x_max) / 2, y_max);
+    ctx.stroke();
+
+    text.value = x_min + ' ' + y_min + ' ' + (x_max - x_min) + ' ' + (y_max - y_min);
 }
